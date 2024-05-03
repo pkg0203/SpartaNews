0ae703ce87bec3aec8013c4aec2886d5bfb4b77b
pkg0203 <pkg0203@naver.com>
2024-05-03 16:32:35 +0900

    댓글 삭제 구현 완료

--- sparta_news/comments/views.py
+++ sparta_news/comments/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.status import *
 from articles.models import Article
 from .serializers import *
 
-# Create your views here.
+
 class CommentView(APIView):
-    def get(self,request,article_id):
-        comment=Comment.objects.filter(article=article_id)
-        serializer = CommentSerializer(comment,many=True)
+    def get(self, request, article_id):
+        comment = Comment.objects.filter(article=article_id)
+        serializer = CommentSerializer(comment, many=True)
         return Response(serializer.data)
+
+    def delete(self, request, comment_id):
+        comment = get_object_or_404(Comment, pk=comment_id)
+        comment.delete()
+        return Response({f'pk : {comment_id} had successfully been deleted.'})
