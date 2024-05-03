0cc9b4c40ef69d63a5857be25337288c93dd7ba2
pkg0203 <pkg0203@naver.com>
2024-05-03 14:32:00 +0900

    대댓글 모델 수정

--- sparta_news/comments/models.py
+++ sparta_news/comments/models.py
@@ -12,9 +12,15 @@ class Comment(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
-
-class Co_Comment(Comment):
+class Co_Comment(models.Model):
     comment_at = models.ForeignKey(
         Comment,
+        related_name='co_comments',
         on_delete=models.CASCADE
     )
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
