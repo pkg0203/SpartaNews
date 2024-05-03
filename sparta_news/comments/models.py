d93ee29485b0b83bcafb0715312c5f80a0e2671a
pkg0203 <pkg0203@naver.com>
2024-05-03 12:44:25 +0900

    comment/co_comment 모델 정의

--- sparta_news/comments/models.py
+++ sparta_news/comments/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Co_Comment(Comment):
+    comment_at = models.ForeignKey(
+        Comment,
+        on_delete=models.CASCADE
+    )
