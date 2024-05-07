c1460df428edead053b54dc22f890d12fe0f775b
LeeJS9856 <wnstj1057@naver.com>
2024-05-07 16:59:07 +0900

    article 1차 병합

--- sparta_news/sparta_news/ai_test.py
+++ sparta_news/sparta_news/ai_test.py
@@ -4,7 +4,7 @@ import config
 client = OpenAI(api_key=config.OPENAI_API_KEY)
 
 system_instructions = """
-너는 기자이고, 주어진 링크의 뉴스를 요약해줘. 만약 뉴스가 영어로 되어있다면, 한글로 번역해서 요약해줘.
+너는 기자이고, 주어진 링크의 뉴스를 마크다운 언어를 사용해서 요약해줘. 만약 뉴스가 영어로 되어있다면, 한글로 번역해서 요약해줘.
 """
 
 completion = client.chat.completions.create(
--- sparta_news/sparta_news/settings.py
+++ sparta_news/sparta_news/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django_extensions',
     'rest_framework',
     'rest_framework_simplejwt.token_blacklist',
+    'markdownx',
     # Local
     'articles',
     'accounts',
