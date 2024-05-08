672512ad6a657f01cf98e0a3fb8cea943ef22187
pkg0203 <pkg0203@naver.com>
2024-05-08 16:27:01 +0900

    작성 후 얼마나 지났는지 보여줌

--- sparta_news/comments/serializers.py
+++ sparta_news/comments/serializers.py
@@ -1,5 +1,24 @@
 from rest_framework import serializers
 from .models import *
+from datetime import datetime, timedelta
+from django.utils.timezone import make_aware, is_aware
+
+
+class RelativeDateField(serializers.Field):
+    def to_representation(self, created_at):
+        now = datetime.now().date()
+        created_at = created_at.date()
+        delta = now - created_at
+        years = delta.days // 365
+        days = delta.days
+        minutes = delta.seconds // 60
+
+        if years > 0:
+            return f"{years} years ago"
+        elif days > 0:
+            return f"{days} days ago"
+        else:
+            return f"{minutes} minutes ago"
 
 
 class CoCommentSerializer(serializers.ModelSerializer):
@@ -13,7 +32,8 @@ class CoCommentSerializer(serializers.ModelSerializer):
 
 class CommentViewSerializer(serializers.ModelSerializer):
     co_comments = CoCommentSerializer(many=True, read_only=True)
-    
+    date = RelativeDateField(source='created_at')
+
     class Meta:
         model = Comment
         fields = "__all__"
