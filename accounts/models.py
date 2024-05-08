71f05ee03efcd1d039c2d7d885c1e667e6b760d4
sumina-codewell <sumina110423@gmail.com>
2024-05-08 13:33:16 +0900

    add serializer

--- accounts/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-# Create your models here.
-class User(AbstractUser):
-    username = models.CharField(max_length=30, unique=True)
-    nickname = models.CharField(max_length=30, unique=True)
