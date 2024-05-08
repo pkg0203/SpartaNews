f2e064813f74e7eeed0b112e2b3bcb85283a5349
sumina-codewell <sumina110423@gmail.com>
2024-05-08 14:48:30 +0900

    modify serializer

---
+++ sparta_news/accounts/serializers.py
@@ -0,0 +1,33 @@
+from rest_framework import serializers
+from .models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'nickname', 'password']
+
+    def validate_password(self, value):
+        if len(value) < 5:
+            raise serializers.ValidationError()
+        if not any(char.isdigit() for char in value):
+            raise serializers.ValidationError()
+        if not any(char.isalpha() for char in value):
+            raise serializers.ValidationError()
+        return value
+
+
+class UserDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['password', 'nickname']
+        read_only_fields = ['username']
+
+    def update(self, instance, validated_data):
+        if self.context['request'].user.id != instance.id:
+            raise serializers.ValidationError()
+
+        instance.password = validated_data.get('password', instance.password)
+        instance.nickname = validated_data.get('nickname', instance.nickname)
+        instance.save()
+        return instance
--- sparta_news/accounts/views.py
+++ sparta_news/accounts/views.py
@@ -1,41 +1,31 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from django.contrib.auth import get_user_model
+from .serializers import *
+from django.shortcuts import get_object_or_404
+from accounts.models import User
 
 # Create your views here.
+
+
 class User(APIView):
+    # create user
     def post(self, request):
-        data = request.data
-        username = data.get('username')
-        nickname = data.get('nickname')
-        password = data.get('password')
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
 
-        if not id or not nickname:
-            return Response(
-                {'error': 'username 및 nickname을 모두 입력해주세요.'},
-                status=400
-            )
+    # modify user info
+    def put(self, request, id):
+        user = get_object_or_404(User, id=id)
+        serializer = UserDetailSerializer(user, data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
 
-        if get_user_model().objects.filter(username=username).exists():
-            return Response(
-                {'error': '이미 존재하는 username입니다.'},
-                status=400
-            )
-
-        if get_user_model().objects.filter(nickname=nickname).exists():
-            return Response(
-                {'error': '이미 존재하는 nickname입니다.'},
-                status=400
-            )
-
-        user = get_user_model().objects.create_user(
-            username=username,
-            nickname=nickname,
-            password=password,
-        )
-        return Response(
-            {"id": user.id,
-            "username": user.username, 
-            "nickname": user.nickname, },
-            status=201
-        )
+    # delete user(
+    def delete(self, request, id):
+        user = get_object_or_404(User, id=id)
+        user.delete()
+        return Response(status=200)
